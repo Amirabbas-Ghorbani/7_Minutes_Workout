9f14b3760c07e96ab969dbe82501f7d68af0eb0e
Amirabbas-Ghorbani <amirabbas.ghorbani67@gmail.com>
2023-05-25 15:54:51 +0200

    first commit

---
+++ app/src/main/java/ghorbani/amir/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,30 @@
+package ghorbani.amir.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import ghorbani.amir.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Here we will start the exercise",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
