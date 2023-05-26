f477fe3051563e7d35fe416a433d84cd8bb0c640
Amirabbas-Ghorbani <amirabbas.ghorbani67@gmail.com>
2023-05-26 16:00:51 +0200

    second commit

---
+++ app/src/main/java/ghorbani/amir/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,26 @@
+package ghorbani.amir.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ghorbani.amir.a7minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding : ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
