d6257b27a5b677e4def66064b46761a8fa4aa28a
Amirabbas-Ghorbani <amirabbas.ghorbani67@gmail.com>
2023-05-28 20:31:38 +0200

    third commit

--- app/src/main/java/ghorbani/amir/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/ghorbani/amir/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package ghorbani.amir.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import ghorbani.amir.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
+    private var restTimer : CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,58 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Now we start the exercise",
+                    Toast.LENGTH_LONG).show()
+                when(restProgress){
+                    10 -> restTimer?.let {
+                        restProgress = 0
+                        binding?.progressBar?.max = 30
+                        binding?.tvTitle?.text = "EXERCISE NAME"
+                        object : CountDownTimer(30000, 1000){
+                            override fun onTick(millisUntilFinished: Long) {
+                                restProgress++
+                                binding?.progressBar?.progress = 30 - restProgress
+                                binding?.tvTimer?.text = (30 - restProgress).toString()
+                            }
+
+                            override fun onFinish() {
+                                Toast.makeText(this@ExerciseActivity, "Exercise is finished",
+                                    Toast.LENGTH_LONG).show()
+                            }
+                        }.start()
+                    }
+                }
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
