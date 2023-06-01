569f113bd5d2a6aa197edebbc48f5faddec9f36a
Amirabbas-Ghorbani <amirabbas.ghorbani67@gmail.com>
2023-06-01 18:17:15 +0200

    fifth commit

--- app/src/main/java/ghorbani/amir/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/ghorbani/amir/a7minutesworkout/ExerciseActivity.kt
@@ -3,11 +3,14 @@ package ghorbani.amir.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import ghorbani.amir.a7minutesworkout.databinding.ActivityExerciseBinding
+import java.util.Locale
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding : ActivityExerciseBinding? = null
     private var restTimer : CountDownTimer? = null
@@ -18,6 +21,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var exerciseList : ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
+
+    private var tts : TextToSpeech? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +42,8 @@ class ExerciseActivity : AppCompatActivity() {
 
         exerciseList = Constants.defaultExerciseList()
 
+        tts = TextToSpeech(this, this)
+
         setupRestView()
     }
 
@@ -46,11 +54,11 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.flExerciseProgressBar?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
 
-
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
+
         setRestProgressBar()
         showUpcomingExercise()
     }
@@ -67,6 +75,8 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
 
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
         binding?.tvExercise?.text = exerciseList!![currentExercisePosition].getName()
@@ -119,6 +129,26 @@ class ExerciseActivity : AppCompatActivity() {
     private fun showUpcomingExercise(){
         binding?.llUpcomingExercise?.visibility = View.VISIBLE
         binding?.tvUpcomingExerciseName?.text = exerciseList!![currentExercisePosition + 1].getName()
+
+        var upcomingExerciseName: String = exerciseList!![currentExercisePosition + 1].getName()
+        speakOut("UPCOMING EXERCISE $upcomingExerciseName")
+    }
+
+    private fun speakOut(text : String){
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val resultEnglish = tts!!.setLanguage(Locale.US)
+
+            if (resultEnglish == TextToSpeech.LANG_MISSING_DATA || resultEnglish == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The specified Language is not supported!")
+
+            } else {
+                Log.e("TTS", "Initialization Failed!")
+            }
+        }
     }
 
     override fun onDestroy() {
@@ -132,6 +162,13 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        if (tts != null){
+            tts?.stop()
+            tts?.shutdown()
+        }
         binding = null
     }
+
+
 }
